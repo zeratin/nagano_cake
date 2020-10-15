3d74a6038473acd2cd368fd5e5c8826f0278e796
EC2 Default User <ec2-user@ip-172-31-60-147.ec2.internal>
2020-10-15 07:33:22 +0000

    start

---
+++ db/migrate/20201015043131_create_genres.rb
@@ -0,0 +1,15 @@
+class CreateGenres < ActiveRecord::Migration[5.2]
+  def change
+    create_table :genres do |t|
+      t.string :name
+      t.boolean :is_active, default: "true"
+      t.timestamp :created_at, default: "now"
+      t.timestamp :updated_at, default: "now"
+      
+      
+
+      t.timestamps
+    end
+  end
+end
+
---
+++ db/migrate/20201015043252_create_orders.rb
@@ -0,0 +1,18 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+            t.integer :shipping_cost
+      t.string :address
+      t.integer :postal_code
+      t.string :name
+      t.string :payment_method, defalut: 0
+      t.integer :status, defalut: 0
+      t.integer :total_payment
+      t.timestamp :created_at, default: "now"
+      t.timestamp :updated_at, default: "now"
+      t.integer :customer_id
+
+      t.timestamps
+    end
+  end
+end
