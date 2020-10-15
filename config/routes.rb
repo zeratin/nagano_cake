d7d1b9a16b63b1f6aa2767c239560dcbc8fe073c
EC2 Default User <ec2-user@ip-172-31-60-147.ec2.internal>
2020-10-15 17:08:52 +0000

    routing fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admins do
     devise_for :admins, controllers:{
       sessions:'admins/sessions',
       registrations:'admins/registrations',
       passwords:'admins/passwords'
     }
-  end
-  scope module: 'customers' do
+
     devise_for :customers, controllers:{
       sessions:'customers/sessions',
       registrations:'customers/registrations',
       passwords:'customers/passwords'
     }
-  end
-  
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_065821) do
+ActiveRecord::Schema.define(version: 2020_10_15_160206) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_065821) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -47,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_065821) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
