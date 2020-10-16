d56e0d1aca57672ce6d03156324aacb989c44530
Tkmt-U <umitakamatsu@gmail.com>
2020-10-16 13:41:30 +0000

    Update Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admins do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admins do
     devise_for :admins, controllers:{
