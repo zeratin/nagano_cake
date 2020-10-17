1965a1cd4ba29bdbe49979428a012f1000fa321c
Tkmt-U <umitakamatsu@gmail.com>
2020-10-17 09:34:59 +0000

    Update Commit(delete name column at customers table)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_150923) do
+ActiveRecord::Schema.define(version: 2020_10_17_093323) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2020_10_16_150923) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name_kana"
@@ -55,6 +54,9 @@ ActiveRecord::Schema.define(version: 2020_10_16_150923) do
     t.string "phone_number"
     t.string "address"
     t.string "postal_code"
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "is_deleted"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
