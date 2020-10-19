e7bc99981a27162a6879246fd4e672a49a1e718b
EC2 Default User <abcdquont1234@gmail.com>
2020-10-19 09:39:14 +0000

    Update item関連

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  before_action :set_genres, only[:new, :eidit, :index, :create, :update]
 
   def new
     @item = Item.new
   end
 
   def create
+   @item = Item.new(item_params)
+    if @item.save
+     flash[:notice] = "新商品を登録しました"
+     redirect_to adin_item_path(@item)
+    else
+      render :new
+    end
   end
 
   def index
-
+   @items = Item.all.page(params[:page]).per(10)
   end
 
   def show
@@ -24,10 +32,10 @@ class Admin::ItemsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :image, :introduction,
-       :genre_id, :tax_out_price, :is_sale)
+       :genre_id, :, :is_active)
   end
 
   def set_genres
-    @genres = Genre.where(is_active: false)
+    @genres = Genre.where(is_active: true)
   end
 end
