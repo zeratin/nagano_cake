3912dd840e1bb3acfb181c14ba37410db24df920
EC2 Default User <ec2-user@ip-172-31-95-113.ec2.internal>
2020-10-27 14:54:20 +0000

    重複商品一部気になる所あり

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,27 +9,30 @@ class CartItemsController < ApplicationController
   def create
     @cart_items = CartItem.all
     @cart_item = CartItem.new(cart_item_params)
-    # @cart_item = CartItem.new(cart_item_params)を@cart_item = CartItem.find(cart_item_params)に間違えるとエラー起きます
     @cart_item.customer_id = current_customer.id
-    search_cart = @cart_items.where(customer_id: current_customer.id).where(item_id: @cart_item.item_id)
 
-    if search_cart.empty?
-      cart_item = CartItem.new(cart_item_params)
-      cart_item.customer_id = current_customer.id
-      cart_item.save
-      redirect_to cart_items_path
-    else
-      binding.pry
-      amount = search_cart[0].amount + @cart_item.amount
-      @cart_item.update(amount: amount)
-      redirect_to cart_items_path
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+      else
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.customer_id = current_customer.id
+      end
     end
-
-
-    # if @cart_item.blank?
-    #
-    #   redirect_to cart_items_path
+    @cart_item.save
+    redirect_to cart_items_path
+    # search_cart = @cart_items.where(customer_id: current_customer.id).where(item_id: @cart_item.item_id)
+    # if search_cart.empty?
+    #   @cart_item = CartItem.new(cart_item_params)
+    #   @cart_item.customer_id = current_customer.id
+    # else
+    #   amount = search_cart[0].amount + @cart_item.amount
+    #   @cart_item.update(amount: amount)
     # end
+    # @cart_item.save
+    # redirect_to cart_items_path
   end
 
   def update
