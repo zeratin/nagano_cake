604414b3f750afbcc0dc0213ba10502e222b3e80
Tkmt-U <umitakamatsu@gmail.com>
2020-10-27 06:51:21 +0000

    Fix Commit(admin top)

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -2,7 +2,7 @@ class Admins::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @order = Order.where(created_at: Date.today).count
+    @order = Order.where(created_at: 0.day.ago.all_day).count
   end
-  
+
 end
